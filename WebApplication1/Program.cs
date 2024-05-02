89c56bd9d0afb6529787d58b3930787a6a3fe4d0
Andrew Casey <amcasey@users.noreply.github.com>
2024-05-02 13:24:35 -0700

    Add an endpoint for dumping data protection key metadata (#1)

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,9 +20,10 @@ app.MapGet("/", () =>
                 <ul>
                     <li><a href="/protect">Protect Data</a></li>
                     <li><a href="/unprotect">Unprotect Data</a></li>
+                    <li><a href="/keys">List Data Protection Keys</a></li>
                 </ul>
             </body>
-    """, 
+    """,
     "text/html");
 });
 
@@ -35,6 +39,41 @@ app.MapGet("/unprotect/{secret}", (string secret, IDataProtectionProvider protec
     var protector = protectionProvider.CreateProtector("MyApp");
     var unprotectedData = protector.Unprotect(secret);
     return new SecretAndInstance(unprotectedData, Environment.MachineName);
+});
+
+app.MapGet("/keys", (IKeyRingProvider keyRingProvider, IKeyManager keyManager) => {
+    var defaultKeyId = keyRingProvider.GetCurrentKeyRing().DefaultKeyId;
+    var allKeys = keyManager.GetAllKeys();
+    var defaultKey = allKeys.Single(k => k.KeyId == defaultKeyId);
+
+    var builder = new StringBuilder();
+    builder.AppendLine($"Machine Name: {Environment.MachineName}");
+    builder.AppendLine();
+
+    builder.AppendLine($"**Default Key**");
+    AppendKey(builder, defaultKey);
+    builder.AppendLine();
+
+    builder.AppendLine($"**Other Keys**");
+    foreach (var key in allKeys)
+    {
+        if (key.KeyId == defaultKeyId)
+        {
+            continue;
+        }
+        AppendKey(builder, key);
+    }
+
+    return builder.ToString();
+
+    static void AppendKey(StringBuilder builder, IKey key)
+    {
+        builder.AppendLine($"Key ID: {key.KeyId:B}");
+        builder.AppendLine($"Creation Date (PDT): {key.CreationDate.ToOffset(TimeSpan.FromHours(-7))}");
+        builder.AppendLine($"Activation Date (PDT): {key.ActivationDate.ToOffset(TimeSpan.FromHours(-7))}");
+        builder.AppendLine($"Expiration Date (PDT): {key.ExpirationDate.ToOffset(TimeSpan.FromHours(-7))}");
+        builder.AppendLine();
+    }
 });
 
 
